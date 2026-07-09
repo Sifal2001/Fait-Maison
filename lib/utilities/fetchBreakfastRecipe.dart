dbac4f155185ead628f5a4017841c23817b2dcbe
Sifal2001 <sifalyak@gmail.com>
2026-07-09 20:03:50 +0200

    Add useFakeData test stub for recipe fetches (no API calls in test mode)

--- lib/utilities/fetchBreakfastRecipe.dart
+++ lib/utilities/fetchBreakfastRecipe.dart
@@ -3,27 +3,58 @@ import 'dart:convert';
 import '../Modals/RecipeAlbum.dart';
 import 'fetchMBRecipeAlbum.dart';
 import 'getBreakfastMenuFromDB.dart';
-import 'getBreakfastPrefFromDB.dart';
 import 'package:http/http.dart' as http;
 
 var br_index;
 
 Future<Recipe> fetchRecipeAlbum() async {
-  final response = await http
-      .get(Uri.parse('https://api.spoonacular.com/recipes/complexSearch?query=${breakfastMenu[br_index]}&number=1&apiKey=fea1e0484037450bb541f4e54a1fc370'));
+
+  if (useFakeData) {
+    return Recipe.fromJson({
+      'title': 'Test Recipe Detail',
+      'image': 'https://via.placeholder.com/556x370',
+      'servings': 2,
+      'summary': 'This is a fake recipe used for testing without API calls.',
+      'spoonacularScore': 57.7,
+      'healthScore': 14,
+      'readyInMinutes': 10,
+      'sourceUrl': 'https://example.com',
+      'nutrition': {
+        'nutrients': [
+          {'name': 'Calories', 'amount': 298.4},
+          {'name': 'Fat', 'amount': 1.1},
+          {'name': 'x', 'amount': 0},
+          {'name': 'x', 'amount': 0},
+          {'name': 'x', 'amount': 0},
+          {'name': 'x', 'amount': 0},
+          {'name': 'x', 'amount': 0},
+          {'name': 'x', 'amount': 0},
+          {'name': 'x', 'amount': 0},
+          {'name': 'Protein', 'amount': 12.0},
+        ],
+      },
+      'extendedIngredients': [
+        {'original': '2 eggs', 'name': 'eggs'},
+        {'original': '1 cup spinach', 'name': 'spinach'},
+      ],
+      'winePairing': null,
+      'diets': ['vegetarian'],
+      'dishTypes': ['breakfast'],
+    });
+  }
+
+  final response = await http.get(Uri.parse(
+      'https://api.spoonacular.com/recipes/complexSearch?query=${breakfastMenu[br_index]}&number=1&apiKey=fea1e0484037450bb541f4e54a1fc370'));
 
   if (response.statusCode == 200) {
     final id_response = jsonDecode(response.body);
     var recipe_id = id_response['results'][0]['id'];
-    final recipe_response = await http
-        .get(Uri.parse('https://api.spoonacular.com/recipes/$recipe_id/information?includeNutrition=true&apiKey=fea1e0484037450bb541f4e54a1fc370'));
+    final recipe_response = await http.get(Uri.parse(
+        'https://api.spoonacular.com/recipes/$recipe_id/information?includeNutrition=true&apiKey=fea1e0484037450bb541f4e54a1fc370'));
 
     final rep_2 = jsonDecode(recipe_response.body);
     return Recipe.fromJson(jsonDecode(recipe_response.body));
-
   } else {
-    // If the server did not return a 200 OK response,
-    // then throw an exception.
     throw Exception('Failed to load');
   }
 }
--- lib/utilities/fetchMBRecipeAlbum.dart
+++ lib/utilities/fetchMBRecipeAlbum.dart
@@ -3,28 +3,53 @@ import 'package:http/http.dart' as http;
 import 'package:login/Modals/MBRecipe.dart';
 import 'getBreakfastMenuFromDB.dart';
 
+const bool useFakeData = true;
 
 Future<MBRecipe> fetchMBRecipeAlbum() async {
-final response = await http
-    .get(Uri.parse('https://api.spoonacular.com/recipes/complexSearch?query=${breakfastMenu[0]}&number=1&apiKey=fea1e0484037450bb541f4e54a1fc370'));
-final rep = jsonDecode(response.body);
-var recipe_id = rep['results'][0]['id'];
-print(recipe_id);
+  // ---- TEST MODE: return fake data, no API call ----
+  if (useFakeData) {
+    return MBRecipe.fromJson({
+      'title': 'Test Omelette',
+      'servings': 2,
+      'spoonacularScore': 57.7,
+      'healthScore': 14,
+      'readyInMinutes': 10,
+      // nutrients must have index 0, 1, and 9 present:
+      'nutrition': {
+        'nutrients': [
+          {'name': 'Calories', 'amount': 298.4}, // [0] calories
+          {'name': 'Fat', 'amount': 1.1},        // [1] fat
+          {'name': 'x', 'amount': 0},             // [2]
+          {'name': 'x', 'amount': 0},             // [3]
+          {'name': 'x', 'amount': 0},             // [4]
+          {'name': 'x', 'amount': 0},             // [5]
+          {'name': 'x', 'amount': 0},             // [6]
+          {'name': 'x', 'amount': 0},             // [7]
+          {'name': 'x', 'amount': 0},             // [8]
+          {'name': 'Protein', 'amount': 12.0},   // [9] protein
+        ],
+      },
+      'extendedIngredients': [
+        {'original': '2 eggs', 'name': 'eggs'},
+        {'original': '1 cup spinach', 'name': 'spinach'},
+      ],
+      'diets': ['vegetarian'],
+      'dishTypes': ['breakfast'],
+    });
+  }
 
-if (response.statusCode == 200) {
-  final response_2 = await http
-      .get(Uri.parse('https://api.spoonacular.com/recipes/$recipe_id/information?includeNutrition=true&apiKey=fea1e0484037450bb541f4e54a1fc370'));
+  final response = await http.get(Uri.parse(
+      'https://api.spoonacular.com/recipes/complexSearch?query=${breakfastMenu[0]}&number=1&apiKey=fea1e0484037450bb541f4e54a1fc370'));
+  final rep = jsonDecode(response.body);
+  var recipe_id = rep['results'][0]['id'];
+  print(recipe_id);
 
-  final rep_2 = jsonDecode(response_2.body);
-
-  return MBRecipe.fromJson(rep_2);
-
-} else {
-  // If the server did not return a 200 OK response,
-  // then throw an exception.
-  throw Exception('Failed to load');
+  if (response.statusCode == 200) {
+    final response_2 = await http.get(Uri.parse(
+        'https://api.spoonacular.com/recipes/$recipe_id/information?includeNutrition=true&apiKey=fea1e0484037450bb541f4e54a1fc370'));
+    final rep_2 = jsonDecode(response_2.body);
+    return MBRecipe.fromJson(rep_2);
+  } else {
+    throw Exception('Failed to load');
+  }
 }
-}
-
-
-
